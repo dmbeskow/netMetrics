d734c5a8d86512dfba29c8cde807c9c4a424b861
David Beskow <dnbeskow@gmail.com>
2020-01-24 05:00:17 -0500

    added dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
 setup(name='netMetrics',
-      version='0.1',
+      version='0.2',
       description='Functions to automate network feature creation',
       url='http://github.com/dbeskow/netMetrics',
       author='David Beskow',
       author_email='dnbeskow@gmail.com',
       license='MIT',
       packages=['netMetrics'],
+      install_requires=[
+              'tweepy',
+              'pandas',
+              'progressbar2',
+              'networkx',
+              'numpy',
+              'python-louvain'
+              ],
       zip_safe=False)
